3fe5159d062db7cc3c5c5ae259e0a162144b5f21
jeffspatafora <jeffspatafora@gmail.com>
2022-04-02 11:11:57 -0700

    adds problem 1 work

--- arrays_select_practice_ruby.rb
+++ arrays_select_practice_ruby.rb
@@ -1,6 +1,17 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
+numbers = [7, 4, 33, 80, 6, 10, 88, 19]
+new_numbers = []
+numbers.each do |number|
+  if number < 20
+    new_numbers << number
+  end
+end
+
+p new_numbers
+
+
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
