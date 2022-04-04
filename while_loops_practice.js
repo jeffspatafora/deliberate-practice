e496dd73e587100d579da0fcf7c55a406ec58784
jeffspatafora <jeffspatafora@gmail.com>
2022-04-03 22:35:43 -0700

    adds problem 2 work

--- while_loops_practice.js
+++ while_loops_practice.js
@@ -1,12 +1,19 @@
 // # 1. Write a while loop to print the numbers 1 through 10.
 
-let number = 1;
-while (number <= 10) {
-  console.log(number);
-  number++;
-}
+// let number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
 
 // # 2. Write a while loop that prints the word "hello" 5 times.
+
+let word = "hello";
+let i = 0;
+while (i < 5) {
+  console.log(word);
+  i++;
+}
 
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
