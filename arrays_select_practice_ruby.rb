5c1168d931f899cc35ccb784cfeb38f96635aa39
jeffspatafora <jeffspatafora@gmail.com>
2022-04-03 21:45:40 -0700

    adds problem 8 work using while and .select loop

--- arrays_select_practice_ruby.rb
+++ arrays_select_practice_ruby.rb
@@ -111,33 +111,53 @@
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
-items = [
-  {name: "chair", price: 100}, 
-  {name: "pencil", price: 1}, 
-  {name: "book", price: 4}
-]
+# items = [b
+#   {name: "chair", price: 100}, 
+#   {name: "pencil", price: 1}, 
+#   {name: "book", price: 4}
+# ]
 
-new_items = []
+# new_items = []
 
-items.each do |item|
-  if item[:name].length < 6
-    new_items << item
-  end
-end
+# items.each do |item|
+#   if item[:name].length < 6
+#     new_items << item
+#   end
+# end
 
-p new_items
+# p new_items
 
-new_items = items.select do |item|
-  item[:name].length < 6
-end
+# new_items = items.select do |item|
+#   item[:name].length < 6
+# end
 
-p new_items
+# p new_items
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+strings = ["big", "little", "good", "bad", "Baseball", "Hockey", "football"]
+
+new_strings = []
+
+i = 0
+while i < strings.length
+  if strings[i][0].downcase != "b"
+    new_strings << strings[i]
+  end
+  i += 1
+end
+
+p new_strings
+
+new_strings = strings.select do |string|
+  string[0].downcase != "b"
+end
+
+p new_strings
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
