17e6ddfdce4f6760d4f3e4ac4fc55deeabba32d2
jeffspatafora <jeffspatafora@gmail.com>
2022-04-03 21:31:21 -0700

    adds problem 5 work using while and .select loop

--- arrays_select_practice_ruby.rb
+++ arrays_select_practice_ruby.rb
@@ -88,8 +88,30 @@
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
+strings = ["a", "man", "a", "plan", "a", "canal", "panama", "history", "phrases", "are", "fun"]
+new_strings = []
+
+i = 0
+while i < strings.length
+  if strings[i].length < 4
+    new_strings << strings[i]
+  end
+  i += 1
+end
+
+p new_strings
+
+
+new_strings = strings.select do |string|
+  string.length < 4
+end
+
+p new_strings
+
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
