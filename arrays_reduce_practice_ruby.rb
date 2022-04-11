fd63e1fd0e0fb7f22f827f68d8c613195d1cb3d1
jeffspatafora <jeffspatafora@gmail.com>
2022-04-10 19:54:40 -0700

    adds problem 7 work using while, .reduce, .each loop

--- arrays_reduce_practice_ruby.rb
+++ arrays_reduce_practice_ruby.rb
@@ -135,24 +135,51 @@
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
-items = [{name: "chair", price: 100}, {name: "pencil", price: 50}, {name: "book", price: 4}]
+# items = [{name: "chair", price: 100}, {name: "pencil", price: 50}, {name: "book", price: 4}]
 
 
-lowest_price = items[0]
-i = 0
-while i < items.length
-  if lowest_price[:price] > items[i][:price]
-    lowest_price = items[i]
-  end
-  i += 1
-end
+# lowest_price = items[0]
+# i = 0
+# while i < items.length
+#   if lowest_price[:price] > items[i][:price]
+#     lowest_price = items[i]
+#   end
+#   i += 1
+# end
 
-p lowest_price
+# p lowest_price
 
 
 
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
+
+numbers = [10, 40, 70, 90]
+product = 1
+i = 0
+while i < numbers.length
+  product = product * numbers[i]
+  i += 1
+end
+
+p product
+
+product = numbers.reduce(1) { |product, number| product * number }
+
+p product
+
+product = numbers.reduce(1) do |product, number|
+  product * number 
+end
+
+p product
+
+product = 1
+numbers.each do |number|
+  product = product * number
+end
+
+p product
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
