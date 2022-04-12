671094d69753e73d6fb61790aad4b6ae35e0b518
jeffspatafora <jeffspatafora@gmail.com>
2022-04-11 21:49:18 -0700

    adds problem 1 work

--- methods_practice_problems.js
+++ methods_practice_problems.js
@@ -1,5 +1,8 @@
 // # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
+var word = window.prompt("please enter a word: ");
+console.log(word.toUpperCase());
+
 // # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
 // # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
