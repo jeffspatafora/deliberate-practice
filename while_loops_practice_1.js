6b7721ab460b7b02d53a779795f1246da636fbf9
jeffspatafora <jeffspatafora@gmail.com>
2022-04-11 21:23:42 -0700

    adds problem 2 work

--- while_loops_practice_1.js
+++ while_loops_practice_1.js
@@ -9,7 +9,12 @@
 
 // # 2. Write a while loop that prints the word "hello" 5 times.
 
-
+// let string = "hello";
+// let count = 1;
+// while (count <= 5) {
+//   console.log(string);
+//   count++;
+// }
 
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
