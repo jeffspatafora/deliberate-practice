08ad50f21e182f66f16022117b7eea6587fe5b07
jeffspatafora <jeffspatafora@gmail.com>
2022-05-30 01:04:21 -0700

    adds problem 2 work for ruby converting data practice problems

--- converting_data_practice_problems.rb
+++ converting_data_practice_problems.rb
@@ -23,7 +23,7 @@
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
-hash = [
+array = [
   {
     id: 1, 
     color: "blue", 
@@ -35,6 +35,16 @@ hash = [
     price: 12
   }
 ]
+
+hash = {}
+
+i = 0
+while i < array.length
+  hash[array[i][:id]] = array[i]
+  i += 1
+end
+
+p hash
 
 
 
