95d63aa333e1ecf468e0a2160fa6d84d596d0753
jeffspatafora <jeffspatafora@gmail.com>
2022-05-30 00:21:48 -0700

    adds problem 5 work for ruby-methods-practice

--- js-while-loops-practice1.js
+++ js-while-loops-practice1.js
@@ -2,14 +2,16 @@
 
 // # 2. Write a while loop that prints the word "hello" 5 times.
 
-let word = "hello";
-let count = 1;
-while (count <= 5) {
-  console.log(word);
-  count++;
-}
+// let word = "hello";
+// let count = 1;
+// while (count <= 5) {
+//   console.log(word);
+//   count++;
+// }
 
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+
 
 // # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
--- ruby-methods-practice.rb
+++ ruby-methods-practice.rb
@@ -6,11 +6,19 @@
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
-puts "enter a word: "
-word = gets.chomp
-puts word.reverse
+# puts "enter a word: "
+# word = gets.chomp
+# puts word.reverse
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+puts "enter a word: "
+word = gets.chomp
+i = 1
+10.times do
+  puts "#{word} #{i}"
+  i += 1
+end
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
