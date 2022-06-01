2209b17e53c9f9012db2e7156f62a18eda007161
jeffspatafora <jeffspatafora@gmail.com>
2022-06-01 16:02:12 -0700

    adds problem 5 work

--- javascript-practice-22-6-1.js
+++ javascript-practice-22-6-1.js
@@ -33,15 +33,27 @@
 
 // # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
-let number = 9001;
+// let number = 9001;
 
-if (number > 9000) {
-  console.log(1);
+// if (number > 9000) {
+//   console.log(1);
+// } else {
+//   console.log(-1);
+// }
+
+// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+let number = 29;
+
+if (number < 10) {
+  console.log(9);
+} else if (number < 20) {
+  console.log(19);
+} else if (number < 30) {
+  console.log(29);
 } else {
   console.log(-1);
 }
-
-// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
 // # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
