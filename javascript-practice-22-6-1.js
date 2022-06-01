d5149d39cefcb888f15d03f172fff9842cea3e22
jeffspatafora <jeffspatafora@gmail.com>
2022-06-01 15:39:57 -0700

    adds problem 2 work

--- javascript-practice-22-6-1.js
+++ javascript-practice-22-6-1.js
@@ -1,14 +1,24 @@
 // # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-let number = 10;
+// let number = 10;
 
-if (number === 10) {
-  console.log(0);
-} else {
-  console.log(-1);
-}
+// if (number === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
 
 // # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+let number = 20;
+
+if (number < 10) {
+  console.log(-1);
+} else if (number > 10) {
+  console.log(1);
+} else if (number === 10) {
+  console.log(0);
+}
 
 // # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
