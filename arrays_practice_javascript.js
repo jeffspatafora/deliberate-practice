21de9ceeb5f4313295d2f74433176746360d6441
jeffspatafora <jeffspatafora@gmail.com>
2022-07-19 00:32:07 -0700

    adds problem 4 and 5 work

--- arrays_practice_javascript.js
+++ arrays_practice_javascript.js
@@ -1,17 +1,39 @@
 // # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-let words = ["time", "for", "coding"];
-words.push("it's");
-words.push("fun");
-console.log(words);
+// let words = ["time", "for", "coding"];
+// words.push("it's");
+// words.push("fun");
+// console.log(words);
 
 // # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
+// let letters = ["a", "b", "c", "d"];
+// letters[1] = 1;
+// console.log(letters);
+
 // # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+// let numbers = [1, 2, 3, 4, 5];
+// let i = 0;
+// while (i < numbers.length) {
+//   console.log(numbers[i]);
+//   i++;
+// }
 
 // # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
+// let numbers = [1];
+// numbers.push(2);
+// numbers.push(3);
+// numbers.push(4);
+// console.log(numbers);
+
 // # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+let strings = ["how", "are", "you"];
+strings[2] = strings[2].toUpperCase();
+console.log(strings);
+
 
 // # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
