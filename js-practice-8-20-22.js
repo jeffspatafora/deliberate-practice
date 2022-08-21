bb7ba6e9db74158f744ed26484219ed688a4022e
jeffspatafora <jeffspatafora@gmail.com>
2022-08-20 20:24:33 -0400

    adds javascript if statement practice

---
+++ js-practice-8-20-22.js
@@ -0,0 +1,51 @@
+// # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+let number1 = 10;
+
+if (number1 === 10) {
+  console.log(0);
+} else {
+  console.log(-1);
+}
+
+// # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+let number2 = 10;
+
+if (number2 < 10) {
+  console.log(-1);
+} else if (number2 > 10) {
+  console.log(1);
+} else if (number2 === 10) {
+  console.log(0);
+}
+
+// # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+// # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+// # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+// # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+// # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+// # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+// # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+// # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+const number3 = 21;
+
+if (number3 > 80) {
+  console.log(5);
+} else if (number3 > 60) {
+  console.log(4);
+} else if (number3 > 40) {
+  console.log(3);
+} else if (number3 > 20) {
+  console.log(2);
+} else {
+  console.log(1);
+}
