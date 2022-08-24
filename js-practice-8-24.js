e1df47ad8a7816c8c312c9ff0100dfd1b77d8492
jeffspatafora <jeffspatafora@gmail.com>
2022-08-24 15:05:37 -0400

    adds js practice

---
+++ js-practice-8-24.js
@@ -0,0 +1,57 @@
+// # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+const words = ['one', 'two', 'three'];
+
+words.push('four');
+words.push('five');
+
+console.log(words);
+
+// # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+const letters = ['a', 'b', 'c', 'd'];
+
+letters[1] = 1;
+
+console.log(letters);
+
+
+
+// # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+const numbers = [0, 1, 2, 3, 4];
+
+let i = 0;
+while (i < numbers.length) {
+  console.log(numbers[i]);
+  i++;
+}
+
+// # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+const numbers1 = [1];
+
+numbers1.push(2);
+numbers1.push(3);
+numbers1.push(4);
+
+console.log(numbers1);
+
+
+// # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+const strings = ['one', 'two', 'three'];
+
+strings[2] = strings[2].toUpperCase();
+
+console.log(strings);
+
+// # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+// # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+// # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+// # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+// # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
