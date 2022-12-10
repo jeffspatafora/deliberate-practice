8a20a720233c939c65c2f264168d5a448603554e
jeffspatafora <jeffspatafora@gmail.com>
2022-12-09 18:18:27 -0800

    adds practice

--- js-functions-practice-problems.js
+++ js-functions-practice-problems.js
@@ -16,8 +16,23 @@ console.log(allCaps('hello world'));
 
 // # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+function minusTheSecond(number1, number2) {
+  console.log(number1 - number2);
+}
+
+minusTheSecond(10, 5);
+minusTheSecond(10, 2);
+minusTheSecond(10, 9);
 // # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
+function timesItself(number) {
+  console.log(number * number);
+}
+
+timesItself(4);
+timesItself(6);
+timesItself(9);
+timesItself(10);
 // # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
 // # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
